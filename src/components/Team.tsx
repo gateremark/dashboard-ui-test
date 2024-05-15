75a944a40a24953337a7edc860b9d6ac2841337e
gateremark <mark5gatere@gmail.com>
2024-05-16 01:52:39 +0300

    refactor: Update Team component header font weight

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -3,7 +3,7 @@ import { teamData } from "../constants";
 const Team = () => {
   return (
     <div className=" my-6 px-4 py-6 flex flex-col gap-3 shadow-md bg-white rounded-xl">
-      <h2 className="text-xl font-medium">Team</h2>
+      <h2 className="text-xl font-semibold">Team</h2>
       <div className="grid grid-cols-3 gap-4">
         <div className="flex items-center gap-4 cursor-pointer">
           <span className=" text-2xl w-[50px] h-[50px] p-2 rounded-full border-2 border-dashed text-gray-500 flex justify-center items-center">
