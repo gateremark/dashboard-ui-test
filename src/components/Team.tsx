2e507c22740ea0d4f1c84c1bb783bd78a78f0488
gateremark <mark5gatere@gmail.com>
2024-05-16 13:09:38 +0300

    refactor: Update Team component grid layout for better responsiveness

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -4,7 +4,7 @@ const Team = () => {
   return (
     <div className=" my-6 px-4 py-6 flex flex-col gap-3 shadow-md bg-white rounded-xl">
       <h2 className="text-xl font-semibold">Team</h2>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         <div className="flex items-center gap-4 cursor-pointer">
           <span className=" text-2xl w-[50px] h-[50px] p-2 rounded-full border-2 border-dashed text-gray-500 flex justify-center items-center">
             +
